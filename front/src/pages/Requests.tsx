fc4673535f83edad14ebfe64d2750a1b3070720a
Rafael Freitas <vidarafael@hotmail.com>
2021-06-12 20:49:30 -0300

    arrumando o filter no requests

--- front/src/pages/Requests.tsx
+++ front/src/pages/Requests.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Link} from 'react-router-dom'
 import {useSelector} from 'react-redux'
 
 interface IFoodNew {
+  id: string,
   nome: string, 
   custo: number, 
   food: string,
@@ -11,13 +12,20 @@ interface IFoodNew {
 
 function Requests() {
   const comidas = useSelector(state => state) as IFoodNew[] 
+  const novaArr  = comidas.filter((it, i) =>  comidas.indexOf(it) === i)
+  useEffect(() => {
+    console.log(comidas)
+    console.log("esse", novaArr)
+  }, [comidas])
   return (
     <>
       <h1>Todos os pedidos</h1>
       <Link to="/menu/pratos"><button>Voltar</button></Link>
-      {comidas.map((it: IFoodNew) => { 
-        return <li>{it.nome}, Preço: {it.custo}, quantidade: {it.quantidade}</li>
-      })}
+      <ul>
+        {novaArr.map((it:IFoodNew ) => { 
+          return <li> Nome: {it.nome} <br/>Quantidade: {it.quantidade}<br/> Valor: {it.custo}<br/><br/></li>
+        })}
+      </ul>
     </>
   )
 }
--- front/src/reducer/foodReducer.ts
+++ front/src/reducer/foodReducer.ts
@@ -1,21 +1,21 @@
 /* eslint-disable import/no-anonymous-default-export */
-interface IFood {
-  id: string,
-  nome: string, 
-  custo: number, 
-  food: string
-  quantidade: number
-}
+// interface IFood {
+//   id: string,
+//   nome: string, 
+//   custo: number, 
+//   food: string
+//   quantidade: number
+// }
 
 // interface Action {
 //   type: string
 //   payload: IFood
 // }
 
-function foodReducer(state: IFood[] = [], action: any) {
+function foodReducer(state:any = [], action: any) {
   switch (action.type) {
     case 'ADD_REQUEST':
-      return state.find(item => item.id === action.payload.id)
+      return [...state, action.payload]
   
     default:
       return state;
